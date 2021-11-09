4fca76a855bb3f53e5e359b213484b3dd5679274
tlips <thomas17.lips@gmail.com>
2021-11-09 17:05:51 +0000

    formatting

--- src/cartesian_waypoint_execution/include/cartesian_waypoint_execution/trajectory_processing.hpp
+++ src/cartesian_waypoint_execution/include/cartesian_waypoint_execution/trajectory_processing.hpp
@@ -1,7 +1,6 @@
 #include <moveit/robot_trajectory/robot_trajectory.h>
 
-namespace trajectory_processing {
-
-void updateTrajectory(robot_trajectory::RobotTrajectory &rob_trajectory,
-                      const std::vector<double> &time_diff);
-} // namespace trajectory_processing
+namespace trajectory_processing
+{
+void updateTrajectory(robot_trajectory::RobotTrajectory& rob_trajectory, const std::vector<double>& time_diff);
+}  // namespace trajectory_processing
